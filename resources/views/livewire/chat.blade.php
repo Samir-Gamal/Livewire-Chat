4e9f2edebb0da1c69cd8b4e6b7f37068f8ac5096
Samir Gamal <samir.gamal77@yahoo.com>
2021-05-24 15:47:18 +0300

    first commit

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,62 +1,61 @@
 <div wire:poll>
-<div class="container">
-  <h3 class=" text-center">  
-    
-    @if(auth()->user()->email == "samir.gamal77@yahoo.com")
-    <a class="btn btn-primary" href="{{Url('delete_chat')}}">حذف المحادثة</a>
-    @endif
-        
-    مورا سوفت 
-  
-  </h3>
- 
-  <div class="messaging">
-        <div  class="inbox_msg">
-          <div  class="mesgs">
-            <div id="chat" class="msg_history">
-              @forelse ($messages as $message)
+    <div class="container">
+        <h3 class=" text-center">
 
-              @if($message->user->name == auth()->user()->name)
-                  <!-- Reciever Message-->
-                      <div class="outgoing_msg">
-                        <div class="sent_msg">
-                          <p>{{ $message->message_text }}</p>
-                          <span class="time_date"> {{  $message->created_at->diffForHumans(null, false, false) }}</span> </div>
-                         
+            @if (auth()->user()->email == 'samir.gamal77@yahoo.com')
+                <a class="btn btn-primary" href="{{ Url('delete_chat') }}">حذف المحادثة</a>
+            @endif
+            مورا سوفت
+        </h3>
+        <div class="messaging">
+            <div class="inbox_msg">
+                <div class="mesgs">
+                    <div id="chat" class="msg_history">
+                        @forelse ($messages as $message)
 
-                      </div>
-                      
-              @else
-                
-                      <div class="incoming_msg">{{ $message->user->name }}
-                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                        <div class="received_msg">
-                          <div class="received_withd_msg">
-                            <p>{{ $message->message_text }}</p>
-                            <span class="time_date">{{  $message->created_at->diffForHumans(null, false, false) }}</span></div>
+                            @if ($message->user->name == auth()->user()->name)
+                                <!-- Reciever Message-->
+                                <div class="outgoing_msg">
+                                    <div class="sent_msg">
+                                        <p>{{ $message->message_text }}</p>
+                                        <span class="time_date">
+                                            {{ $message->created_at->diffForHumans(null, false, false) }}</span>
+                                    </div>
+                                </div>
+
+                            @else
+
+                                <div class="incoming_msg">{{ $message->user->name }}
+                                    <div class="incoming_msg_img"> <img
+                                            src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                    <div class="received_msg">
+                                        <div class="received_withd_msg">
+                                            <p>{{ $message->message_text }}</p>
+                                            <span
+                                                class="time_date">{{ $message->created_at->diffForHumans(null, false, false) }}</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            @endif
+                        @empty
+                            <h5 style="text-align: center;color:red"> لاتوجد رسائل سابقة</h5>
+                        @endforelse
+
+                    </div>
+                    <div class="type_msg">
+                        <div class="input_msg_write">
+                            <form wire:submit.prevent="sendMessage">
+                                <input onkeydown='scrollDown()' wire:model.defer="messageText" type="text"
+                                    class="write_msg" placeholder="اكتب رسالتك" />
+                                <button class="msg_send_btn" type="submit">ارسال</button>
+                            </form>
                         </div>
-                      </div>
+                    </div>
 
-                  @endif
-              @empty
-                 <h5 style="text-align: center;color:red"> لاتوجد رسائل سابقة</h5>
-              @endforelse
+                </div>
+            </div>
 
-            </div>
-            <div class="type_msg">
-              <div class="input_msg_write">
-                <form wire:submit.prevent="sendMessage">
-                  <input onkeydown='scrollDown()' wire:model.defer="messageText" type="text" class="write_msg" placeholder="اكتب رسالتك" />
-                  <button class="msg_send_btn" type="submit">ارسال</button>
-                </form>
-              </div>
-            </div>
-        
-          </div>
         </div>
-    
-      </div>
     </div>
-  </div>
-
-
+</div>
